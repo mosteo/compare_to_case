b83f83c3fa20af33337088483b44a97b74345505
Alejandro R. Mosteo <amosteo@unizar.es>
2024-06-04 15:41:41 +0200

    Make generic the function only

--- src/compare_to_case.ads
+++ src/compare_to_case.ads
@@ -1,15 +1,14 @@
-generic
-   type Comparable (<>) is limited private;
-   with function "<" (L, R : Comparable) return Boolean;
 package Compare_To_Case is
 
    type Result is (Left_First, Equal, Right_First);
 
-   function Is_Same (L, R : Comparable) return Boolean
-   is (not (L < R) and then not (R < L));
-   --  If the provided "<" does not honor this property, the result of the
-   --  following compare function will be wrong.
+   -------------
+   -- Compare --
+   -------------
 
+   generic
+      type Comparable (<>) is limited private;
+      with function "<" (L, R : Comparable) return Boolean is <>;
    function Compare (L, R : Comparable) return Result
    --  This is the purpose of this crate, which is to get in one call the three
    --  possible outcomes of comparing two values.
@@ -19,5 +18,17 @@ package Compare_To_Case is
         else (if R < L
               then Right_First
               else Equal));
+
+   ----------------
+   -- Which_True --
+   ----------------
+
+   function Which_True (L, R : Boolean) return Result
+   is (if L and then not R then
+          Left_First
+       elsif R and then not L then
+          Right_First
+       else
+          Equal);
 
 end Compare_To_Case;
